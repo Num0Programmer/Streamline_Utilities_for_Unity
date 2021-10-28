dec1391d178613091ccec4563fbbc9d6a0365659
Lenin Valdivia <59747577+Oqqay@users.noreply.github.com>
2021-10-28 14:59:44 -0700

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    //refs to adjacent nodes
+    public Node top;
+    public Node bottom;
+    public Node right;
+    public Node left;
+    public GameObject tile;
+
+    public float xPos, zPos;
+
+    //Initialization constructor
+    public Node(float xPos, float zPos, string tag)
+    {
+        //instantiate new tile with given x and z pos
+        this.xPos = xPos;
+        this.zPos = zPos;
+
+        //check for if this current GameObject tile wants to be root
+        if(tag == "root")
+        {
+            tile = GameObject.FindWithTag("root");
+        }
+
+        //otherwise, assume tile is not root
+        else
+        {
+            tile = GameObject.FindWithTag("Tile");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public Node genNewTile(float xPos, float zPos)
+    {
+        /*
+        //check for this is equal to root
+        if(this == TileGenerator.getRootRef())
+        {
+            top = genNewTile(xPos, zPos + transform.localScale.x * 2);
+
+            bottom = genNewTile(xPos, zPos - transform.localScale.x * 2);
+
+            left = genNewTile(xPos - transform.localScale.x * 2, zPos);
+
+            right = genNewTile(xPos + transform.localScale.x * 2, zPos);
+        }
+        
+
+        */
+        return this;
+    }
+    
+}
---
+++ TileGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+
+    Node rootTile;
+    public PlayerController player;
+    bool generated = false;
+    [SerializeField] private static int OFFSET = 20;
+    
+
+    //Initialization constructor
+    public TileGenerator()
+    {
+        //instantiate root tile to null
+        rootTile = null;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //instantiate new node for root tile
+        startingTiles();
+        Instantiate(rootTile.tile, new Vector3(rootTile.xPos, 0, rootTile.zPos), Quaternion.identity);
+        Instantiate(rootTile.right.tile, new Vector3(rootTile.right.xPos, 0, rootTile.right.zPos), Quaternion.identity);
+        Instantiate(rootTile.left.tile, new Vector3(rootTile.left.xPos, 0, rootTile.left.zPos), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if root tile points to tile that player is on
+        //utilizes a function within player contoller that does the raycasting
+        if (player.notOnRootTile())                          //this was just to test to see if i can make a new one on the right side         
+        {
+            rootTile.left = rootTile;
+            rootTile = rootTile.right;
+            rootTile.right = new Node(rootTile.xPos + OFFSET, 0, "Tile");
+
+            Instantiate(rootTile.right.tile, new Vector3(rootTile.right.xPos, 0, rootTile.right.zPos), Quaternion.identity);
+            setNewRootRef(); 
+        }
+        
+    }
+
+    public Node getRootRef()
+    {
+        //return if this ref is equal to root tile
+
+        return rootTile;
+        //return null;
+    }
+
+    public void setNewRootRef()
+    {
+        //check for if root ref moved
+        if (getRootRef().tile.tag == "Tile")
+        {
+            //set new ref's tag back to root
+            getRootRef().tile.tag = "root";
+
+            rootTile.left.tile.tag = "Tile";
+            rootTile.right.tile.tag = "Tile";
+        }
+    }
+    
+    public void startingTiles()
+    {
+        rootTile = new Node(0, 0, "root");
+
+        rootTile.right = new Node(OFFSET, 0, "Tile");
+        rootTile.left = new Node(-OFFSET, 0, "Tile");
+    }
+    
+}
