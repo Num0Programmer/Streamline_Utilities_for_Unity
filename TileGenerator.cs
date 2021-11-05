e93fcffd16f9dee8001bbeee5d852ce4192d704f
Num0Programmer <56742774+Num0Programmer@users.noreply.github.com>
2021-11-05 16:15:11 -0700

    Update TileGenerator.cs
    Removed inclusion of System.Collections and System.Collections.Generic libraries.

--- TileGenerator.cs
+++ TileGenerator.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///     Author: Oqquay
+/// </summary>
 public class TileGenerator : MonoBehaviour
 {
 
